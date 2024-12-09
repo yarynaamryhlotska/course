56650c1d2de5c11ff2332fa5bca9a8c4066999e1
yarynaamryhlotska <yarynamryhlotskaa@gmail.com>
2024-12-09 20:12:20 +0000

    new

--- DotnetApiPostgres.Api/Program.cs
+++ DotnetApiPostgres.Api/Program.cs
@@ -34,7 +34,7 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 // Додавання маршруту для кореневої сторінки
-app.MapGet("/", () => "Welcome to DotnetApiPostgres API! Now with a new message! i am Yarynax");
+app.MapGet("/", () => "Welcome to DotnetApiPostgres API! Now with a new message!");
 
 app.Run();
 
